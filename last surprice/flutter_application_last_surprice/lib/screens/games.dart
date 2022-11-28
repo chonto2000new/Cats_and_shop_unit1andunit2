21b13ad2c2d0816a8954c87bcdf1f18b3f98d03d
AGUSTIN ALBERTO  MONARREZ HERRERA <agustin.monarrez.is@unipolidgo.edu.mx>
2022-11-27 21:16:37 -0600

    Uni1_and_Unit2
    The one and  two

---
+++ last surprice/flutter_application_last_surprice/lib/screens/games.dart
@@ -0,0 +1,150 @@
+import '../flutter_flow/flutter_flow_theme.dart';
+import '../flutter_flow/flutter_flow_util.dart';
+import '../flutter_flow/flutter_flow_widgets.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/scheduler.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+class GamesWidget extends StatefulWidget {
+  const GamesWidget({Key? key}) : super(key: key);
+
+  @override
+  _GamesWidgetState createState() => _GamesWidgetState();
+}
+
+class _GamesWidgetState extends State<GamesWidget> {
+  final scaffoldKey = GlobalKey<ScaffoldState>();
+
+  @override
+  void initState() {
+    super.initState();
+    // On page load action.
+    SchedulerBinding.instance.addPostFrameCallback((_) async {});
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      key: scaffoldKey,
+      backgroundColor: FlutterFlowTheme.of(context).primaryBackground,
+      body: SafeArea(
+        child: GestureDetector(
+          onTap: () => FocusScope.of(context).unfocus(),
+          child: Padding(
+            padding: EdgeInsetsDirectional.fromSTEB(10, 0, 0, 0),
+            child: SingleChildScrollView(
+              child: Column(
+                mainAxisSize: MainAxisSize.max,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Row(
+                    mainAxisSize: MainAxisSize.max,
+                    children: [
+                      Padding(
+                        padding: EdgeInsetsDirectional.fromSTEB(0, 0, 240, 0),
+                        child: FFButtonWidget(
+                          onPressed: () async {},
+                          text: '',
+                          icon: Icon(
+                            Icons.dehaze,
+                            color: FlutterFlowTheme.of(context).primaryText,
+                            size: 15,
+                          ),
+                          options: FFButtonOptions(
+                            width: 50,
+                            height: 50,
+                            color: Color(0x004B39EF),
+                            textStyle:
+                                FlutterFlowTheme.of(context).subtitle2.override(
+                                      fontFamily: 'Poppins',
+                                      color: Colors.white,
+                                    ),
+                            borderSide: BorderSide(
+                              color: Colors.transparent,
+                              width: 1,
+                            ),
+                          ),
+                        ),
+                      ),
+                      FFButtonWidget(
+                        onPressed: () async {},
+                        text: '',
+                        icon: Icon(
+                          Icons.arrow_back_sharp,
+                          color: FlutterFlowTheme.of(context).primaryText,
+                          size: 15,
+                        ),
+                        options: FFButtonOptions(
+                          width: 40,
+                          height: 40,
+                          color: Colors.white,
+                          textStyle:
+                              FlutterFlowTheme.of(context).subtitle2.override(
+                                    fontFamily: 'Poppins',
+                                    color: Colors.white,
+                                  ),
+                          borderSide: BorderSide(
+                            color: Colors.transparent,
+                            width: 1,
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                  Column(
+                    mainAxisSize: MainAxisSize.max,
+                    children: [
+                      Row(
+                        mainAxisSize: MainAxisSize.max,
+                        children: [
+                          Image.network(
+                            'https://picsum.photos/seed/850/600',
+                            width: 100,
+                            height: 100,
+                            fit: BoxFit.cover,
+                          ),
+                          Padding(
+                            padding:
+                                EdgeInsetsDirectional.fromSTEB(10, 0, 0, 0),
+                            child: Text(
+                              'Name',
+                              style: FlutterFlowTheme.of(context).bodyText1,
+                            ),
+                          ),
+                          Padding(
+                            padding:
+                                EdgeInsetsDirectional.fromSTEB(20, 0, 0, 0),
+                            child: Text(
+                              'price',
+                              style: FlutterFlowTheme.of(context).bodyText1,
+                            ),
+                          ),
+                          Padding(
+                            padding:
+                                EdgeInsetsDirectional.fromSTEB(20, 0, 0, 0),
+                            child: Text(
+                              'console',
+                              style: FlutterFlowTheme.of(context).bodyText1,
+                            ),
+                          ),
+                          Padding(
+                            padding:
+                                EdgeInsetsDirectional.fromSTEB(10, 0, 0, 0),
+                            child: Text(
+                              'Genres',
+                              style: FlutterFlowTheme.of(context).bodyText1,
+                            ),
+                          ),
+                        ],
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
